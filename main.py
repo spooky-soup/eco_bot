30756d9e52bfaeaaaab67670a985cb18f41106c6
Sofya Mikhaylenko <vgeser.13@ya.ru>
2023-12-04 15:07:58 +0700

    dbs

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import telebot
+import pymysql
+
 bot = telebot.TeleBot('6483038185:AAEkPUufyhe4PotSf4a-6z9Mj61zunLMVGU')
+
+conn = pymysql.connect(
+    host='localhost',
+    user='admin',
+    password="admin",
+)
 
 # метод для получения текстовых сообщений
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "/start":
         bot.send_message(message.from_user.id, "Привет, это ЭкоБот.")
+    elif message.text == "/dbs":
+        cur = conn.cursor()
+        cur.execute("SHOW TABLES FROM mysql;")
+        strlist = []
+        for x in cur:
+            strlist.append(str(x) + "\n")
+        bot.send_message(message.from_user.id, ''.join(strlist))
     elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Для начала работы напиши /start.")
+        bot.send_message(message.from_user.id, "Для начала работы напиши /start." + "\n" +
+                         "Для вывода всех табличек напиши /dbs.")
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
